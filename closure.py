ca31b9f7eb3ca597bb7ab87261414d61d3f7b1e9
MichaelKl04 <mikeykleiman@gmail.com>
2024-02-08 18:19:55 -0500

    Updated

--- closure.py
+++ closure.py
@@ -1,4 +1,4 @@
-#Memorization using Closures: Implement a memoization decorator using closures.
+#Memorization using Closures: Implement a memorization decorator using closures.
 #The decorator should cache the results of a function for particular arguments 
 #and return the cached result when the function is called with those arguments again, instead of recomputing it.
 
